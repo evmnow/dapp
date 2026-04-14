310230a1b3da4eda4c59c58f5f086c60af5397c3
jalil <jalil@1001.digital>
2026-04-14 23:33:05 +0200

    Add better intro

--- packages/dapp/app/components/AppSearchPanel.vue
+++ packages/dapp/app/components/AppSearchPanel.vue
@@ -20,8 +20,30 @@
             <span>Search</span>
           </Button>
         </FormInputGroup>
-        <p class="muted">Read and interact with smart contrats on Ethereum.</p>
       </FormGroup>
+      <aside>
+        <h1>Smart Contract Reader</h1>
+        <p class="muted">
+          This is a streamlined open source version of the full block explorer
+          experience at <a href="https://evm.now">evm.now</a>. We believe EVM
+          contracts deserve a simple, open source UI that enables users to use
+          contracts permissionlessly. That's why we're working on a
+          <a
+            href="https://github.com/evmnow/contract-metadata/"
+            target="_blank"
+            rel="noopener"
+            >standard</a
+          >
+          and
+          <a
+            href="https://github.com/evmnow/sdk"
+            target="_blank"
+            rel="noopener"
+            >sdk</a
+          >
+          to power better UX for interacting with smart contracts.
+        </p>
+      </aside>
     </form>
   </section>
 </template>
@@ -54,9 +76,19 @@ async function openContract() {
       max-inline-size: calc(var(--content-width-sm) - var(--size-10));
       width: 100%;
       margin-inline: auto;
+    }
 
-      p {
-        padding-inline: var(--size-3);
+    aside {
+      padding-inline: var(--size-1);
+
+      a {
+        color: var(--primary);
+        text-decoration: underline;
+        text-underline-offset: 0.2em;
+
+        &:hover {
+          text-decoration: none;
+        }
       }
     }
 
